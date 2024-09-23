8d150167f60680b33c743923ad87e32eedf856c6
Almudena M. Castro <almudena.m.castro@gmail.com>
2024-09-23 21:11:11 +0200

    add more solutions discovered during the lab review

--- lab-4.4-solution.sql
+++ lab-4.4-solution.sql
@@ -1,6 +1,7 @@
 -- ## Challenge
 
 -- Write SQL queries to perform the following tasks using the Sakila database:
+USE sakila;
 
 -- 1. Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
 
@@ -52,6 +53,23 @@ LEFT JOIN city ci ON a.city_id = ci.city_id
 LEFT JOIN country co ON ci.country_id = co.country_id
 WHERE co.country = 'Canada';
 
+-- Solution without joins (more efficient, using subqueries):
+SELECT first_name, last_name, email
+FROM customer
+WHERE address_id IN (
+    SELECT address_id
+    FROM address 
+    WHERE city_id IN (
+        SELECT city_id 
+        FROM city
+        WHERE country_id IN (
+            SELECT country_id
+            FROM country
+            WHERE country = 'Canada'
+        )
+    )
+);
+
 -- 6. Determine which films were starred by the most prolific actor in the Sakila database. 
 -- A prolific actor is defined as the actor who has acted in the most number of films. 
 -- First, you will need to find the most prolific actor and then use that actor_id to find the different films that he or she starred in.ORDER BY total_films DESC
@@ -67,6 +85,21 @@ FROM (
 LEFT JOIN film_actor fa ON g.actor_id = fa.actor_id
 LEFT JOIN film f ON fa.film_id = f.film_id;
 
+-- Another solution: 
+SELECT f.title
+FROM film f
+JOIN film_actor fa ON f.film_id = fa.film_id
+WHERE fa.actor_id = (
+    SELECT actor_id
+    FROM film_actor
+    GROUP BY actor_id
+    ORDER BY COUNT(film_id) DESC
+    LIMIT 1
+);
+
+
+
 -- 7. Find the films rented by the most profitable customer in the Sakila database. You can use the customer and payment tables to find the most profitable customer, i.e., the customer who has made the largest sum of payments.
+
 
 -- 8. Retrieve the client_id and the total_amount_spent of those clients who spent more than the average of the total_amount spent by each client. You can use subqueries to accomplish this.
